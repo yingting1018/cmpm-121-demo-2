fdf2cdd163d5e039c0402728027dcf5ae8914392
yingting1018 <yhuan300@ucsc.edu>
2024-10-13 22:36:34 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,22 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const gameName = "Sticker Sketchpad";
+document.title = gameName;
+
+const header = document.createElement("h1");
+header.innerHTML = gameName;
+app.append(header);
+
+
+const canvas: HTMLCanvasElement = document.createElement('canvas');
+canvas.className = 'canvas-class';
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+if (ctx) 
+{
+    ctx.fillStyle = 'transparent'; 
+}
